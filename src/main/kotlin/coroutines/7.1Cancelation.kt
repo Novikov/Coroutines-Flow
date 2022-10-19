70d38635e95160a3e0cf9740a2bf69a1da34736f
Igor Novikov <igor.novikov@umbrellait.com>
2022-10-19 13:37:08 +0300

    Cancelation

---
+++ src/main/kotlin/coroutines/7.1Cancelation.kt
@@ -0,0 +1,80 @@
+package coroutines
+
+import kotlinx.coroutines.*
+
+suspend fun main() {
+//    cancellationExample1()
+//    cancellationExample1_1()
+//    cancellationExample1_2()
+    cancellationExample1_3()
+}
+
+/**
+ *Пример отмены корутины.
+ * job.cancel() не является suspend функцией и может быть вызывана где угодно.
+ * Тоже самое можно сделать если вручную создать scope и вызывать cancel на нем.
+ * */
+suspend fun cancellationExample1() = coroutineScope {
+    val job = launch {
+        repeat(10) { index ->
+            println("operation number $index")
+            delay(100)
+        }
+    }
+    delay(250)
+    println("canceling coroutine")
+    job.cancel()
+}
+
+/**
+ * Если заменить delay внутри launch на Thread sleep, то отмена не произойдет. Причина в том, что если вызывается suspend функция (например delay)
+ * на coroutine scope который уже в состоянии cancelling то будет брошен особый CancellationException, который прервет выполнение корутины.
+ * Данный Exception обрабатывать не обязательно, но можно если надо. В примере ниже нет suspend функции поэтоу выполнение не прервется.
+ * */
+suspend fun cancellationExample1_1() = coroutineScope {
+    val job = launch {
+        repeat(10) { index ->
+            delay(1)
+            println("operation number $index")
+            Thread.sleep(100)
+        }
+    }
+    delay(250)
+    println("canceling coroutine")
+    job.cancel()
+}
+
+/**
+ * Способы остановки работы внутри корутины
+ * 1.Вызов suspend функций ensureActive(), yield(), delay()
+ * 2.Проверка состояния корутины с помощью оборота всего блока в if(isActive)
+ * */
+
+suspend fun cancellationExample1_2() = coroutineScope {
+    val job = launch {
+        repeat(10) { index ->
+            yield()
+            delay(1)
+            println("operation number $index")
+            Thread.sleep(100)
+        }
+    }
+    delay(250)
+    println("canceling coroutine")
+    job.cancel()
+}
+
+suspend fun cancellationExample1_3() = coroutineScope {
+    val job = launch {
+        repeat(10) { index ->
+            if (isActive) {
+                delay(1)
+                println("operation number $index")
+                Thread.sleep(100)
+            }
+        }
+    }
+    delay(250)
+    println("canceling coroutine")
+    job.cancel()
+}
