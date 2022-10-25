cf6aadb7e9448891b8f64db600ef1a5525b2e9f1
Igor Novikov <igor.novikov@umbrellait.com>
2022-10-25 19:50:51 +0300

    async

--- src/main/kotlin/coroutines/5.Async.kt
+++ src/main/kotlin/coroutines/5.Async.kt
@@ -6,7 +6,8 @@ suspend fun main() {
 //    asyncExample1()
 //    asyncExample2()
 //    asyncExample3()
-    asyncExample4()
+//    asyncExample4()
+    asyncExample5()
 }
 
 /** async запускает отдельную корутину, которая выполняется параллельно с остальными корутинами. Тоже самое что и Launch*
@@ -72,4 +73,15 @@ suspend fun asyncExample4() = coroutineScope {
 fun sumLazy(a: Int, b: Int): Int {
     println("Coroutine has started")
     return a + b
+}
+
+/** Полезная техника маппинга списка через async*/
+suspend fun asyncExample5() = coroutineScope {
+    val idList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val dataList = idList.toMutableList().map { async { getDataById(it) } }.awaitAll()
+    println(dataList)
+}
+suspend fun getDataById(id: Int): String {
+    delay(100)
+    return "Data by id - $id"
 }
