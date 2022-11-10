6eb85b690f2640b30128eed5cd54f7d7965f013b
Igor Novikov <igor.novikov@umbrellait.com>
2022-11-10 15:06:13 +0300

    coroutine builders 2

--- src/main/kotlin/coroutines/5.Coroutine_Builders_2.kt
+++ src/main/kotlin/coroutines/5.Coroutine_Builders_2.kt
@@ -1,9 +1,11 @@
 package coroutines
 
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
-suspend fun main(){
-    coroutineBuilders2Example1()
+suspend fun main() {
+//    coroutineBuilders2Example1()
+    coroutineBuilders2Example2()
 }
 
 /** Полезная техника маппинга списка через async
@@ -11,11 +13,33 @@ suspend fun main(){
  * */
 suspend fun coroutineBuilders2Example1() = coroutineScope {
     val idList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    val dataList = idList.toMutableList().map { async { getDataById(it) } }.awaitAll()
-    println(dataList)
+    val measuredTime = measureTimeMillis {
+        val dataList = idList.toMutableList().map { async { getDataById(it) } }.awaitAll()
+        println(dataList)
+    }
+    println(measuredTime)
 }
 
 suspend fun getDataById(id: Int): String {
     delay(1000)
     return "Data by id - $id"
+}
+
+/** Тоже самое можно сделать с помощью launch
+ * Здесь есть отличие от async в том, что если в launch передать start параметр CoroutineStart.Lazy - то асинхронная работа прекратится и все будет
+ * выполняться последовательно. Это происходит потому что в launch в это случае Lazy старта необходимо руками вызвать start() на экземпляре job.
+ * В Async start вызовется без нас при вызове await. Соответственно чтоб работало нужно руками вызывать start. Смотри закоментированный пример.
+ * */
+suspend fun coroutineBuilders2Example2() = coroutineScope {
+    val idList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val measuredTime = measureTimeMillis {
+        idList.toMutableList().map { launch { makeRequestByValue(it) } }.joinAll()
+//        idList.toMutableList().map { launch(start = CoroutineStart.LAZY) { makeRequestByValue(it) }.apply { start() } }.joinAll()
+    }
+    println(measuredTime)
+}
+
+suspend fun makeRequestByValue(id: Int) {
+    delay(1000)
+    println("request for id $id")
 }
