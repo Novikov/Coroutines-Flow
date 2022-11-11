715b5e734870e62848ef73de0d0341012444c42d
Igor Novikov <igor.novikov@umbrellait.com>
2022-11-11 12:06:07 +0300

    tasks

--- src/main/kotlin/Tasks.kt
+++ src/main/kotlin/Tasks.kt
@@ -2,13 +2,9 @@
  * 0)Конспект по многопоточности, deadlock
  * 1)Working with shared state
  * 2)Github example
- * 3)Async/Concurrency
- * 4)CoroutineStart
- * 5)join joinAll await awaitAll
- * 6)вызов корутин на разных скоуп. Пример Миши на runBlocking
- * 7)Стейт Машина
- * 8)Старт корутины на main
- * 9)Unconfined dispatcher
- *
- * Проверить будет ли фризиться UI если запускать на main потоке множество запросов в сеть.
+ * 3)Async/Concurrency концепции
+ * 4)вызов корутин на разных скоуп. Пример Миши на runBlocking
+ * 5)Стейт Машина
+ * 6)Старт корутины на main
+ * 7)Проверить будет ли фризиться UI если запускать на main потоке множество запросов в сеть.
  * */
