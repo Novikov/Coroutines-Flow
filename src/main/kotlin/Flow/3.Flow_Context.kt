db72436b6b96ced608d08cb137303717e57a49c1
Igor Novikov <igor.novikov@umbrellait.com>
2022-11-14 18:15:27 +0300

    flow context

--- src/main/kotlin/Flow/3.Flow_Context.kt
+++ src/main/kotlin/Flow/3.Flow_Context.kt
@@ -10,13 +10,14 @@ suspend fun main() {
 
 suspend fun getSmallFlow() = flow {
     for (i in 1..10) {
+        //withContext(..){} // если обернуть код ниже в withContext - бдует краш.
         emit(i)
         println(currentCoroutineContext())
         delay(500)
     }
 }
 
-/** Flow примет контекст того места откуда был вызван collect*/
+/** Flow примет контекст того места откуда был вызван collect. Если это будет не так (Например обернуть эмиссию с помощью withContext) - будет краш*/
 suspend fun flowContextExample1() = coroutineScope {
     withContext(CoroutineName("Caller method context") + Dispatchers.Default) {
         getSmallFlow().collect {
