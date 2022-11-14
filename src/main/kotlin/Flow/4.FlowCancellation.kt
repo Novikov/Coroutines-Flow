ea27f1ed52c41e53b460925e1ae51838174e2f81
Igor Novikov <igor.novikov@umbrellait.com>
2022-11-14 16:32:24 +0300

    Flow cancellation

--- src/main/kotlin/Flow/4.FlowCancellation.kt
+++ src/main/kotlin/Flow/4.FlowCancellation.kt
@@ -1,12 +1,17 @@
 package Flow
 
+import kotlinx.coroutines.NonCancellable.cancel
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.withTimeoutOrNull
 
 suspend fun main() {
-    cancelFlowExample1()
+//    cancelFlowExample1()
+//    cancelFlowExample2()
+    cancelFlowExample3()
 }
 
 suspend fun getSimpleFlow() = flow {
@@ -32,9 +37,32 @@ suspend fun getSimpleFlow() = flow {
     delay(500)
 }
 
-suspend fun cancelFlowExample1() = coroutineScope{
-    withTimeoutOrNull(1000){
-        getSimpleFlow().collect{ value -> println(value)}
+/** Отмена по таймауту*/
+suspend fun cancelFlowExample1() = coroutineScope {
+    withTimeoutOrNull(1000) {
+        getSimpleFlow().collect { value -> println(value) }
         println("done")
+    }
+}
+
+/** Отмена по значению*/
+suspend fun cancelFlowExample2() = coroutineScope {
+    withTimeoutOrNull(1000) {
+        getSimpleFlow().collect { value ->
+            println(value)
+            if (value == 3) cancel()
+        }
+        println("done")
+    }
+}
+
+/** Некоторые операторы не имеют обработчика Cancellation Exception.
+ * Если попытаемся отменить flow то выбросится cancellation exception, который не остановит flow и выбросится в обработчик выше*/
+suspend fun cancelFlowExample3() = coroutineScope {
+    (1..5).asFlow().collect { value ->
+        if (value == 3) {
+            cancel()
+        }
+        println(value)
     }
 }
